c904fb16d207664d6e2ebd0c41eb6a967ba9e855
Rob Hayward <robohay@gmail.com>
2016-11-20 17:39:26 +0000

    Started looking at the IMF data and json link

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ONSdata
 =======
 
-This includes a series of functions to download and manipulate ONS data
+This includes a series of functions to download and manipulate data.  
+
+My latest line is to get data from the IMF using the API.  I have looked at the information on [IMF help page](http://datahelp.imf.org/knowledgebase/articles/667681-using-json-restful-web-service).  There is some basic code in json_test.R.  So far I have just made a list of the main data sets.  
--- Raw.R
+++ Raw.R
@@ -3,7 +3,7 @@
 #tsd-quarterly-national-accounts--q3-2013.html
 # Change the data for latest data and re-name GDP
 # Read annual data
-da <- read.csv("C:/Users/Toshiba/Desktop/GDP.csv ",header = TRUE, nrows = 65) 
+da <- read.csv("Data/GDP.csv",header = TRUE, nrows = 65) 
 plot(da$NRJS[40:80] ~ da[40:80,1], type = 'l', main = "Savings Ratio", 
 xlab = "Time", ylab = "Ratio of Savings to Income")
 # as an example of what can be done. 
@@ -11,7 +11,7 @@ dat <- as.ts(da, start = 1948, end = 2012, frequency = 1)
 str(dat)
 plot(dat[,2])
 # Read quarterly data
-dq <- read.csv("C:/Users/Toshiba/Desktop/GDP.csv ",header = TRUE, 
+dq <- read.csv("Data/GDP.csv",header = TRUE, 
 skip = 93, nrows = 234) 
 dqt <- as.ts(dq, start = c(1955,1), end = c(2013,3), frequency = 4)
 head(dq[,1:3])
@@ -20,7 +20,8 @@ str(dqt)
 plot.ts(dqt[,2])
 # The time series property does not work. 
 # Load the names
-lab <- read.csv("C:/Users/Toshiba/Desktop/GDP.csv ", skip = 332, header = FALSE)
+lab <- read.csv("Data/GDP.csv", skip = 332, 
+                header = FALSE, stringsAsFactors = FALSE)
 head(lab)
 # extLab function will extract the information about the data from the code.
 extLab <- function(name){
@@ -31,24 +32,19 @@ return(a)
 extLab("ABMD")
 # Now try to find code for the phrase.  First tried a code and pmatch but
 # this does not yet work. 
-name <- "saving"
+#http://cran.r-project.org/web/packages/stringr/stringr.pdf
+require(stringr)
+name <- "savings"
 extCod <- function(name){
 a <- pmatch(name, lab[,2])
-which(lab[,2] == name)
-a <- lab[a,]
-class(lab[,2])
-return(a)
+return(lab[a,])
 }
-a
-name
-extCod("savings")
-pmatch("", "")                             # returns NA
-pmatch("m",   c("mean", "median", "mode")) # returns NA
-pmatch("med", c("mean", "median", "mode")) # returns 2
 
-pmatch(c("", "ab", "ab"), c("abc", "ab"), dup = FALSE)
-pmatch(c("", "ab", "ab"), c("abc", "ab"), dup = TRUE)
-## compare
-charmatch(c("", "ab", "ab"), c("abc", "ab"))
-?as.string
-?subset
+extCod("Index")
+
+name <- "Household"
+pattern <- lab[,2]
+a <- str_detect(name, ignore.case(pattern))
+tail(pattern)
+FUN <- str_detect(name, lab[,2])
+sum(FUN)
